70516af4fb8f9b230a6294a2e770ea1600c1a0cd
Sk8056 <sarangkumar8056@gmail.com>
2020-11-25 11:57:58 +0530

    upto followers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        {/* Home page - Dashboard*/}
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        {/* Login page - Login */}
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        {/* Wrong path - Error */}
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
